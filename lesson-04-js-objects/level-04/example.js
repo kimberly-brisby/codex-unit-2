bf39391c41fd4d2393355e5b0e31ee6f5bf8a610
Kimberly Brisby <cappykim7@yahoo.com>
2026-04-23 19:25:38 -0500

    done

--- lesson-04-js-objects/level-04/example.js
+++ lesson-04-js-objects/level-04/example.js
@@ -1,5 +1,8 @@
 // TODO: Default-export an object literal using literal syntax.
 // Example: export default { a: 1, b: "x" };
-export default {
-  /* TODO: add keys and values */
-};
+const student = {
+  level: 2,
+  firstname: "Kenzie",
+  lastname:"Brisby",
+}
+export default student;
