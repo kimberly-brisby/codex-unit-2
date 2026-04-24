0052b42c91027042ca132ff5582c3f67df8fec01
Kimberly Brisby <cappykim7@yahoo.com>
2026-04-23 20:05:00 -0500

    decalre functions

--- lesson-04-js-objects/level-09/object.js
+++ lesson-04-js-objects/level-09/object.js
@@ -1,10 +1,7 @@
-// TODO: Declare named functions then assign them as methods on the exported object.
-// Example:
-// function greet(name) { return 'Hi ' + name }
-// const user = { name: 'Ava' }
-// user.greet = greet
-// export default user
-const user = {
-  /* TODO: add name */
-};
+function greet(name){
+  "Hello" + name
+}
+const user = { name = "Harry"}
+  user.greet = greet
+
 export default user;
