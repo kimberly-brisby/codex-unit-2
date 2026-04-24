097419dfd01338c47fd209a002e73e5a1640bf83
Kimberly Brisby <cappykim7@yahoo.com>
2026-04-23 19:32:32 -0500

    properties

--- lesson-04-js-objects/level-05/access.js
+++ lesson-04-js-objects/level-05/access.js
@@ -1,3 +1,8 @@
 // TODO: Demonstrate dot-notation property access and default-export the result/example.
 // Example: const user = { name: 'Ava' }; export default user.name;
-export default null;
+const food ={
+    
+    firstfood: "pizza",
+    secondfood: "burger",
+}
+export default food;
