633ef0108f658a2a42be60dedc54b077a23cf193
Kimberly Brisby <cappykim7@yahoo.com>
2026-04-24 20:20:54 -0500

    updated

--- lesson-04-js-objects/level-11/object.js
+++ lesson-04-js-objects/level-11/object.js
@@ -1,4 +1,8 @@
-// TODO: Build an object from a written specification and export it as default.
+function summary(){
+  return {book.title}
+  By
+  
+}
 // Example spec: book with title, author, pages, and a summary() method assigned from a named function.
 const book = {
   /* TODO: implement spec */
