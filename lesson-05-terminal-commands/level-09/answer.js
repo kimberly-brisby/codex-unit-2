944d1458580ef24836449088c8f44ecd71cfd7da
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-03 15:27:26 -0500

    done 9

--- lesson-05-terminal-commands/level-09/answer.js
+++ lesson-05-terminal-commands/level-09/answer.js
@@ -1,3 +1,3 @@
 // Level 09 — Practice: Navigate and run
 // TODO: Export a string array of commands.
-export default [];
+export default ["I was able to use ls to list all the folders, then I used cd to find the one folder that had the node that I needed."];
