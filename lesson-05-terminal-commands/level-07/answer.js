ea595e4f1bb6e8690a3f1821761fb6c6fc891dbf
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-03 15:13:17 -0500

    good short cut

--- lesson-05-terminal-commands/level-07/answer.js
+++ lesson-05-terminal-commands/level-07/answer.js
@@ -1,3 +1,3 @@
 // Level 07 — Command history (Up/Down arrows)
 // TODO: Use Up/Down or Ctrl+R and write a short note here mentioning "up"/"down" or "ctrl+r".
-export default "";
+export default "By using the up or down botton i can easily find the command i am looking for instead of typing it out each time.";
