a415611c1409bcb774cc9881f3e38fe6a13893aa
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-03 17:42:37 -0500

    go back and finish

--- lesson-06-js-syntax/level-05-declare-arrays/script.js
+++ lesson-06-js-syntax/level-05-declare-arrays/script.js
@@ -3,5 +3,30 @@
 
 // Example:
 // let userNames = ["Ava", "Jon", "Maya"];
+const userNames = ["Kimberly","NeNe","Laisha","Kenzie","Donna"];
 
-let userNames = [];
+const testGrades = [100, 85, 95, 100, 70];
+
+const colors = ["blue","green","orange", "red","pink"];
+
+const states = ["Missouri","Louisisna", "Montana", "New York","Florida"];
+
+const animals = ["bird","dog","cat","elephant","snake"];
+
+const planets = ["Earth", "Mars","Saturn","Jupiter","Venus"];
+
+const kidAges = [2, 5, 10, 11, 12];
+
+const favoriteNumbers = [3, 5, 7, 11, 22];
+
+const birthYears = [1947, 1971, 1983, 2002, 2016];
+
+const channelNumbers = [4, 10 , 32, 105, 122];
+
+export default favoriteNumbers;
+
+console.log(kidAges);
+
+
+
+
