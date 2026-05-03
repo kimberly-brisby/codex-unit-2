c66b85788d855d85b7e2341cc8a533c61c478185
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-03 14:38:19 -0500

    lesson 3 done and tested

--- lesson-05-terminal-commands/level-03/answer.js
+++ lesson-05-terminal-commands/level-03/answer.js
@@ -1,3 +1,3 @@
 // Level 03 — Change directory with cd
 // TODO: Use `cd` to move into a folder and back; describe your commands here.
-export default "";
+export default "I was able to focus simple on just one folder when I used the cd command.";
