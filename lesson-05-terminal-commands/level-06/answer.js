19da3bcc1ef6765c5945c975738979d4364e4e88
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-03 15:10:59 -0500

    done 6

--- lesson-05-terminal-commands/level-06/answer.js
+++ lesson-05-terminal-commands/level-06/answer.js
@@ -1,3 +1,3 @@
 // Level 06 — Run Node.js files with node
 // TODO: Run `node hello.js` and write a short note here mentioning `node`.
-export default "";
+export default "After I make sure I am in the right file, I can select the node file i want and only the message Hello World will pop up.";
