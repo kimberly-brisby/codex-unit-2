54a3640d598d14a708f6636c1eb182dd65df23dd
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-03 20:21:38 -0500

    6-10

--- lesson-06-js-syntax/level-10-declare-functions/script.js
+++ lesson-06-js-syntax/level-10-declare-functions/script.js
@@ -4,3 +4,35 @@
 function getUser() {
   return "returns the current user";
 }
+function getName (){
+  return "returns the current name";
+}
+function getPassword (){
+  return "returns the password";
+}
+function getAge(){
+  return "returns the age";
+}
+function setColor (){
+  return "set color";
+}
+function setTime(){
+  return "set the time";
+}
+function addToCart(){
+  reurn "adds item to cart";
+}
+function playSound(){
+  return "plays current sound";
+}
+function clapOff(){
+  return "claps turn objet off";
+}
+function pushOn (){
+  return "push turns item on";
+}
+function depositCash (){
+  return "deposit cash in account";
+}
+
+default export playSound;
