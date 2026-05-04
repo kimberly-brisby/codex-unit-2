d0dfdc8686a56cec392710bf4e7cf8fbdde2b191
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-04 16:58:47 -0500

    7-5

--- lesson-07-browser-objects/level-05-querySelector/script.js
+++ lesson-07-browser-objects/level-05-querySelector/script.js
@@ -1,7 +1,7 @@
 // Level 05 — querySelector starter
 // TODO: use document.querySelector('#content') or document.querySelectorAll('.item')
 // TODO: log previous content and update nodes (iterate NodeList with forEach)
-
+const pTag = document.querySelector("p");
 // Scaffold:
 // const one = document.querySelector('#content');
 // console.log('before:', one && one.innerHTML);
@@ -10,3 +10,4 @@
 // list.forEach((el, i) => {
 //   // TODO: update each el.innerText
 // });
+pTag.innerText = "Hello World!";
