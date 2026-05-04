47c6d35d9e5df995808133af0af087ba51a5fe07
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-03 19:19:39 -0500

    6-7 go back

--- lesson-06-js-syntax/level-07-declare-objects/script.js
+++ lesson-06-js-syntax/level-07-declare-objects/script.js
@@ -4,6 +4,24 @@
 // Example:
 // let userProfile = { name: "Ava", age: 28, isSubscriber: false };
 
-let userProfile = {};
+const userProfile = {name:"Kim", age: 30, isStudent: true };
+
+const school = {name:"Salmen", year: 2002, hasGraduate: true };
+
+const car = {make:"Chevrolet", model:"Traverse", color:"black" };
+
+const house = {type:"ranch", story: 2, hasYard:true };
+
+const animal = {speices:"elephant", color:"grey", hasFourLegs: true };
+
+const book = {genre:"urban", cover:"black with roses", hasHappyEnd: false };
+
+const bike = {make:"Kawasaki",founded:1878, isJapan: true };
+
+const toy = {type:"lego", color:"multi-color", isSmall: true };
+
+const playground = {material:"wood", ground:"sand", hasSswings:false };
+
+const store = {type:"retell", product:"food", hasclothes:false };
 
 export default userProfile;
