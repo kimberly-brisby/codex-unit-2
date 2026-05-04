39dde1989f37df748b151a0c44bd4bd3458668b3
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-04 16:24:01 -0500

    7-3

--- lesson-07-browser-objects/level-03-innerText/script.js
+++ lesson-07-browser-objects/level-03-innerText/script.js
@@ -1,9 +1,11 @@
 // Level 03 — innerText starter
-// TODO: select an element (e.g. document.getElementById('desc'))
-// TODO: console.log the previous innerText, set a new innerText, then log the new value
+const pTag = document.getElementById(desc); 
 
 // Scaffold:
 // const el = document.getElementById('desc');
 // console.log('before:', el && el.innerText);
 // // TODO: set el.innerText = '...';
 // console.log('after:', el && el.innerText);
+console.log(pTag.innerText);
+pTag.innerText = "Hello World!";
+console.log(pTag.innerText);
