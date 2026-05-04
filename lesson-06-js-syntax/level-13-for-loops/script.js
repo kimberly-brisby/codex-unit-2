b1b942944aeba361196e76942c72f98ba5584836
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-03 21:26:47 -0500

    6-13

--- lesson-06-js-syntax/level-13-for-loops/script.js
+++ lesson-06-js-syntax/level-13-for-loops/script.js
@@ -1,2 +1,37 @@
 // Level 13 — `for` loops practice (10 different ranges)
 // TODO: Write 10 different for loops that print iterated numbers with console.log.
+for (let i = 0; i < 4; i++){
+    console.log(i);
+}
+for (let i = 9; i > 6; i++ ){
+    console.log(i);
+}
+for (let i = 8;i < 7; i++){
+    console.log(i);
+}
+for (let i = 7; i > 10; count = count + 2 ){
+    console.log(i);
+}
+for (let i = 6; i <  8; i++){
+    console.log(i);
+}
+for (let i = 5; i > 7; i++){
+    console.log(i);
+}
+for (let i = 4; i < 2; i++){
+    console.log(i);
+}
+for (let i = 3; i > 8; i++){
+    console.log(i);
+}
+for (let i = 2; i > 5; i++){
+    console.log(i);
+}
+for (let i = 1; i > 7; count = count + 5){
+    console.log(i);
+}
+for (let i = 10; i < 10; i++){
+    console.log(i);
+}
+
+ 
