f4403dc4e542f9a84029643c52218c36b699be4a
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-03 20:33:36 -0500

    6-11

--- lesson-06-js-syntax/level-11-function-calls/script.js
+++ lesson-06-js-syntax/level-11-function-calls/script.js
@@ -6,3 +6,14 @@ export function reportScore(playerName, score) {
 }
 
 const avaScoreReport = reportScore("Ava", 0);
+const amyScoreReport = reportScore("Amy", 13);
+const emilyScorceReport = reportScore("Emily", 6);
+const ashleyScoreRport = reportScore("Ashley", 2);
+const kenzieScoreReport = reportScore("Kenzie", 18);
+const donnaScoreReport = reportScore("Donna", 10);
+const chrissyScoreReport = reportScore("Chrissy", 8);
+const zeeScoreReport = reportScore("Zee", 9);
+const alexScoreRport = reportScore("Alex", 6);
+const brendaScoreReport = reportScore("Brenda", 10);
+
+export default kenzieScoreReport;
