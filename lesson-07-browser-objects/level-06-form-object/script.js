9d08217882e0d36b03177d4b7ac492b095ca40d7
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-05 15:19:51 -0500

    7-6

--- lesson-07-browser-objects/level-06-form-object/script.js
+++ lesson-07-browser-objects/level-06-form-object/script.js
@@ -1,14 +1,8 @@
 // Level 06 — form-object starter
 // TODO: select the form element (e.g. document.getElementById('sample-form'))
 // TODO: log form.id, form.name, form.elements.length, form.enctype, form.className
-const form =document.getElementById("sample-form");
-
-// Scaffold:
-// const form = document.getElementById('sample-form');
-// if (form) {
-//   console.log('id:', form.id);
-//   console.log('name:', form.name);
-//   console.log('elements length:', form.elements.length);
-//   console.log('enctype:', form.enctype);
-//   console.log('className:', form.className);
-// }
+const form = document.querySelector("#sample-form");
+console.log(form.id);
+console.log(form.name);
+console.log(form.elements.length);
+console.log(form.enctype);
