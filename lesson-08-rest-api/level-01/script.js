d8b2ddb8598cb312caecd27a111256625cb54cf8
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-06 12:32:44 -0500

    8-1

--- lesson-08-rest-api/level-01/script.js
+++ lesson-08-rest-api/level-01/script.js
@@ -1,4 +1,5 @@
 // Level 01 starter script
 // TODO: Select a target element and set its innerText to explain REST API.
 const result = document.getElementById("result");
+result.innerText = "Rest API is an interface that communicates between servers where the client makes a requst and gets a response is returned.";
 // Example: result.innerText = '...'; // fill this in during the exercise
