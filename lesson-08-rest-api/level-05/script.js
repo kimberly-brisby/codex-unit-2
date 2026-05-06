c45b190c1ea5b96400f91ade3261f00ec97aadfc
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-06 16:01:33 -0500

    8-5

--- lesson-08-rest-api/level-05/script.js
+++ lesson-08-rest-api/level-05/script.js
@@ -1,13 +1,18 @@
 // Level 05 starter script
 // TODO: Fetch GET https://api.jsoning.com/mock/public/users, await response.json(), and read display the data.
 const form = document.getElementById("sample-form");
-const pUsername = document.getElementById("username");
-const pEmail = document.getElementById("email");
-const pId = document.getElementById("id");
+form.onsubmit = handleSubmit;
+
 
 async function handleSubmit(e) {
   e.preventDefault();
   // TODO: implement fetch and set the three elements' innerText
+const pUsername = document.getElementById("username");
+const pEmail = document.getElementById("email");
+const pId = document.getElementById("id");
+const response = await fetch("http://api.jsoning.com/mock/public/users");
+const json = await response.json();
+const url = response.url;
+const host = response.headers.get("host");
 }
 
-if (form) form.onsubmit = handleSubmit;
