84f6e364dbab69679cda5e62e9372fa7425ce400
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-06 14:05:23 -0500

    8-3

--- lesson-08-rest-api/level-03/script.js
+++ lesson-08-rest-api/level-03/script.js
@@ -1,11 +1,15 @@
 // Level 03 starter script
 // TODO: Use async/await when calling fetch inside the submit handler.
 const form = document.getElementById("sample-form");
-const result = document.getElementById("result");
+form.onsubmit = handleSubmit;
+
 
 async function handleSubmit(event) {
   event.preventDefault();
+  const result = document.getElementById("result");
+  const response = fetch("https://api.jsoning.com/mock/public/users");
+  result.innerText = "async/await function takes time for the value to return and the browser to fully finish running.";
   // TODO: use async/await to call fetch('https://api.jsoning.com/mock/public/users') and then set result.innerText
 }
 
-if (form) form.onsubmit = handleSubmit;
+
