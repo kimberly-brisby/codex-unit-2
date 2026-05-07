ac291387bb341eef1aac4a2aad5a4ecda5f5df63
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-07 12:26:17 -0500

    8-9

--- lesson-08-rest-api/level-09/script.js
+++ lesson-08-rest-api/level-09/script.js
@@ -1,4 +1,10 @@
 // Level 09 starter script
 // TODO: Save a target element and set its innerText to briefly compare GET and POST.
 const result = document.getElementById("result");
+form.onsubmit = handleSubmit
+
+function handleSubmit(event){
+    event.preventDefault();
+    result.innerText = "GET request data and should not change the server, while POST is commonly used to create updated data."
+}
 // Example: result.innerText = 'GET is for reading; POST is for sending data.'
