f4793ffc48e56b1dd37892e559b26f920d96fd26
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-07 13:01:45 -0500

    8-10 go back and finish 1 failed

--- lesson-08-rest-api/level-10/script.js
+++ lesson-08-rest-api/level-10/script.js
@@ -1,13 +1,22 @@
 // Level 10 starter script
 // TODO: POST pre-processed data, parse JSON response, and display three selected values.
 const form = document.getElementById("sample-form");
+form.onsubmit = handleSubmit;
 const v1 = document.getElementById("v1");
 const v2 = document.getElementById("v2");
 const v3 = document.getElementById("v3");
 
-async function handleSubmit(e) {
-  e.preventDefault();
+async function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = {
+    v1:form.elements.v1.value,
+    v2:form.elements.v2.value,
+    v3:form.elements.v3.value,
+  };
+  await fetch("https://api.jsoning.com/mock/public/users",{method:"POST",headers:{"Content-Type":"application/json"}body:JSON.stringify(data)});
+
   // TODO: collect data, POST to echo server, parse response, and insert values into v1/v2/v3
 }
 
-if (form) form.onsubmit = handleSubmit;
+
