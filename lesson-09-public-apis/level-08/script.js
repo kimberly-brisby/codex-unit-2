69951dddae6285d58ae1d4a2b6b1b580ff08108b
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-21 18:53:21 -0500

    9-8

--- lesson-09-public-apis/level-08/script.js
+++ lesson-09-public-apis/level-08/script.js
@@ -5,15 +5,16 @@ async function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
   const data = {
-    api: "cb612559bbf74a23a8917dfff4cdbb96"
+    "api-key": "cb612559bbf74a23a8917dfff4cdbb96"
   };
 
-  const query = new URLSearchParams(data);
-  const response = await fetch("https://api.humorapi.com/memes/random " + "?" + "cb612559bbf74a23a8917dfff4cdbb96");
-  const result = await fetch(url + "?" );
+  const queryString = new URLSearchParams(data);
+  const response = await fetch("https://api.humorapi.com/memes/random" + "?" + "queryString");
+  const result =  await response.json();
   console.log(question) 
 
   // TODO: Create a `data` object with an "api-key" property (quoted when hyphenated)
   // TODO: Convert `data` into a query string and fetch jokes (or send as header if API requires)
   // TODO: Parse response and render a joke into the DOM. On invalid key, show an error message in the page
 }
+const image = document.getElementById("meme");
