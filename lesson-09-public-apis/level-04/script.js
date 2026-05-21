d74dca3a1e03aa5485bb1879eafd84b8e2fccb2b
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-20 20:35:19 -0500

    9-4

--- lesson-09-public-apis/level-04/script.js
+++ lesson-09-public-apis/level-04/script.js
@@ -1,8 +1,15 @@
-const formTag = document.getElementById("queryForm");
-formTag.onsubmit = handleSubmit;
+const form = document.getElementById("queryForm");
+form.onsubmit = handleSubmit;
 
 function handleSubmit(event) {
   event.preventDefault();
+  const form = event.target;
+  const data = {
+    category: form.element.category.value, 
+    difficulty: form.element.difficulty.value,
+  } 
+  const queryString = new URLSearchParams(data);
+  console.log(queryString);
   // TODO: Read form values with formTag.elements and create `data` object
   // TODO: Use `new URLSearchParams(data)` to create a query string and save to `query`
   // TODO: console.log(query)
