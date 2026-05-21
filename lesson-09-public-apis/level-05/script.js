934087e23aaf8b1e086d874da808f6c093399091
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-20 21:33:14 -0500

    changed 9-5 and passed

--- lesson-09-public-apis/level-05/script.js
+++ lesson-09-public-apis/level-05/script.js
@@ -9,9 +9,12 @@ async function handleSubmit(event) {
     ddifficulty: form.elements.ddifficulties.value,
   };
   const queryString = new URLSearchParams(data);
-  const response = await fetch("https://the-triva-api.com/v2/question" + "?" + queryString,);
+  const response = await fetch( "https://the-triva-api.com/v2/question" + "?" + queryString,);
   const result = await response.json();
-  console.log(result);
+  const question = result[0].question.text;
+  console.log(question);
+  const outTag = document.querySelector("#out");
+  outTag.innerHTML = "<h3>" + question + "</h3>";
   // TODO: Build data object from formTag.elements and convert with URLSearchParams
   // TODO: Use async/await with fetch to call https://the-trivia-api.com/v2/questions
   // TODO: Parse `result` and display result[0].question.text into the DOM
