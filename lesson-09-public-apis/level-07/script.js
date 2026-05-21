ea0febd138e4c91b26acec1d926079ab65b81651
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-21 17:08:26 -0500

    9-7 passed

--- lesson-09-public-apis/level-07/script.js
+++ lesson-09-public-apis/level-07/script.js
@@ -1,6 +1,8 @@
 const messageEl = document.getElementById("message");
 
 // TODO: Select the message element and insert a short explanation about API keys
+const message = document.getElementById("message");
+message.innerText = "API keys idenify your app to a service."; 
 if (messageEl) {
   messageEl.innerText =
     "API keys identify your app to a service; do not commit them publicly.";
