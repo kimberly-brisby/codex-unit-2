e3ab08b28d8c659f2b41952435e1784caea06b62
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-21 16:04:13 -0500

    9-6 passed

--- lesson-09-public-apis/level-06/script.js
+++ lesson-09-public-apis/level-06/script.js
@@ -1,9 +1,21 @@
-const formTag = document.getElementById("queryForm");
-formTag.onsubmit = handleSubmit;
+const form = document.getElementById("queryForm");
+form.onsubmit = handleSubmit;
 
 async function handleSubmit(event) {
   event.preventDefault();
-  // TODO: Fetch trivia questions with async/await
+    const form = event.target;
+    const data = {
+      catergory: form.elements.catergory.value,
+      difficulty: form.elements.difficulty.value,
+    }
+    const query = new URLSearchParams;
+    const response = await fetch("https://the-triva-api.com" + "?" + query);
+    const result = await response.json();
+    const question = result[0].question.text;
+    console.log(question);
+    const choiceA = result[0].correctAnswer;
+    const choiceTagA = document.getElementById("choiceA");
+    choiceTagA.innerText = "choiceA";
   // TODO: Save first question to `question` and separate choices into variables
   // TODO: Render question and choices into DOM
 }
