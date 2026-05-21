eda7beb0cd567dee3c3606776ae4205b199c6665
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-21 11:23:18 -0500

    9-4 changes still failed by 1 thing

--- lesson-09-public-apis/level-04/script.js
+++ lesson-09-public-apis/level-04/script.js
@@ -1,15 +1,18 @@
 const form = document.getElementById("queryForm");
 form.onsubmit = handleSubmit;
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
   const data = {
     category: form.element.category.value, 
     difficulty: form.element.difficulty.value,
   } 
-  const queryString = new URLSearchParams(data);
-  console.log(queryString);
+
+  const query = new URLSearchParams(data);
+  const response = await fetch ( "https://the-triva-api.com/v2/question" );
+  const result = await response.json();
+  console.log(query);
   // TODO: Read form values with formTag.elements and create `data` object
   // TODO: Use `new URLSearchParams(data)` to create a query string and save to `query`
   // TODO: console.log(query)
