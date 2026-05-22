9e4404bb394951c5b9a1eac9c22406273e8a2b34
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-22 17:07:26 -0500

    10-4 done

--- lesson-10-postman/level-04/script.js
+++ lesson-10-postman/level-04/script.js
@@ -1,4 +1,4 @@
 // TODO: Starter for Level 04: POST request basics.
 
 // Students should use Postman to send a POST; this file is a placeholder
-console.log("TODO: log one documentation fact here");
+console.log("Instead of getting random facts for many subjects when the code is typed correctly you only be dircted to on subject and one level of difficulty.");
