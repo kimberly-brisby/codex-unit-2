cb4f77546f7a0ba4846bbec24b89f947c6564c1b
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-22 20:02:45 -0500

    10-7 will have to revisit

--- lesson-10-postman/level-07/script.js
+++ lesson-10-postman/level-07/script.js
@@ -1,4 +1,4 @@
 // TODO: Starter for Level 07: API POST practice.
 
 // Students should construct a JSON payload and send it to their chosen API.
-console.log("TODO: log one documentation fact here");
+console.log("I did not know what I am doing wrong when i hit post. A error message keeps popping up i will review withan Ta to debug this issue.");
