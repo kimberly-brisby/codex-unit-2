af1d787a4d2c825b9530ec401b82fab8f4c9fcee
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-22 19:55:56 -0500

    10-6 done

--- lesson-10-postman/level-06/script.js
+++ lesson-10-postman/level-06/script.js
@@ -2,4 +2,4 @@
 
 // Students should replace the URL below with their chosen API endpoint.
 // TODO: fetch('https://api.example.com/endpoint')
-console.log("TODO: log one documentation fact here");
+console.log("I choose an anime API.I was lost at first on which link to copy. I then found their API Rest link and copy and pasted it. It ssaid 200 ok in green adn listed informantion on of the websites on where things are posted on. ");
