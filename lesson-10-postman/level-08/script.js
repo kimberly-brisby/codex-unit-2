1d7650ac3a67c5805ab24310ca58f64b9c639be1
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-22 20:37:57 -0500

    10-8 I  have to come back I have error in code

--- lesson-10-postman/level-08/script.js
+++ lesson-10-postman/level-08/script.js
@@ -1,1 +1,15 @@
 // TODO: Starter for Level 08: implement submit handler.
+const form = document.getElementById("api-form");
+form.onSubmit = handleSubmit;
+
+async function handleSubmit(event);
+event.preventDefault;
+const form = event.target;
+const data = {
+    name: form.elements.name.value,
+};
+const dataString = new URLSearchParams;
+const response = await fetch("https://api.disneyapi.dev/character" + "?" + datastring);
+const result = await response.json();
+console.log("Who's that character?");
+
