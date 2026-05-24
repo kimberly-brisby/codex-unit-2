15337f66e33a9f92c3f9014a2613b338b634c0b0
Kimberly Brisby <cappykim7@yahoo.com>
2026-05-23 19:02:59 -0500

    10-5

--- lesson-10-postman/level-05/script.js
+++ lesson-10-postman/level-05/script.js
@@ -1,4 +1,4 @@
 // TODO: Starter for Level 05: POST with JSON payloads.
 
 // Placeholder: students should set Content-Type and send JSON in Postman.
-console.log("I did someting worng keep getting bad request code go back the help from TA.");
+console.log("I got a error code and looked up to see which one would work.");
