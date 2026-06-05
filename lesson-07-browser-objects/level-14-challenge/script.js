167189d9272eb917cb735e9c82e39352783d1394
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-05 08:31:32 -0500

    compled all of 14 and passed

--- lesson-07-browser-objects/level-14-challenge/script.js
+++ lesson-07-browser-objects/level-14-challenge/script.js
@@ -1,2 +1,15 @@
-// TODO: Build a small form handler that reads values, conditionally resets the form, and updates the output element.
-// Keep logic small and focused for the challenge.
+const form = document.getElementById("#sample-form");
+form.onsubmit = handleSubmit;
+
+function hanldeSubmit(event){
+    event.preventDefault();
+    const form = event.target();
+
+    const lastName = form.elements.lastName.value;
+    const password = form.elements.password.value;
+
+    if(lastName === "Shobe"){
+        form.reset();
+    }
+    console.log("form-reset scaffold");
+}
