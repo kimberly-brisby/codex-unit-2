6158fe0f96508ebe02ec42e7371de9de27db1c8f
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-05 08:17:33 -0500

    fixed the spelling errors and console log and passed

--- lesson-07-browser-objects/level-11-textarea-range/script.js
+++ lesson-07-browser-objects/level-11-textarea-range/script.js
@@ -4,17 +4,11 @@ const form = document.getElementById("sample-form");
 form.onsubmit = handleSubmit;
 
 function handleSubmit(event) {
-  evevnt.prevevntDefault;
+  event.preventDefault();
    const form = event.target;
    const message = form.elements.message.value;
    const volume = Number(form.elements.volume.value);
-  console.log("textarea & range, vol");
+  console.log("message , volume");
 }
 
-// const form = document.getElementById('sample-form');
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const message = form.elements.message.value;
-//   const volume = Number(form.elements.volume.value);
-//   console.log(message, volume);
-// }
+
