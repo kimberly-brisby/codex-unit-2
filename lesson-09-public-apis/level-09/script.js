9c626601124384a30ed5f26440fe2720a41b330b
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-05 09:49:09 -0500

    9-9 challenge is complete and passed

--- lesson-09-public-apis/level-09/script.js
+++ lesson-09-public-apis/level-09/script.js
@@ -1,7 +1,10 @@
 const formTag = document.getElementById("challengeForm");
 formTag.onsubmit = handleSubmit;
+const errorE1 = document.getElementById("error");
+const succesE1 = document.getElementById("succes");
 
 async function handleSubmit(event) {
+  try{
   event.preventDefault();
   const form = event.target;
   // TODO: Construct `data` from formTag.elements
@@ -20,12 +23,17 @@ async function handleSubmit(event) {
   
   const result = await response.json();
   const message = result.message;
-
+  const firstName = result.firstName;
   // TODO: Save formatted data into `dataString` and send via fetch using async/await
   // TODO: Parse `result` and update the page; handle errors with try/catch
   const errorTag = document.getElementById("error");
   const successTag = document.getElementById("success");
 
- 
+  if(!response.ok)
+    throw new Error("HTTP" + response.status);
+    const result = await response.json();
   
+}catch(error)
+console.log(error)
+errorE1.innerText = "try again";
 }
